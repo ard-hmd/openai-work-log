838df30cf76b2f43c88fdb3fec6b181a0b21665f
ardhmd <ard.datascientest@gmail.com>
2023-09-05 23:18:14 +0000

    add execution time and session

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
 import requests
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session
+from pymongo import MongoClient
+import time
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+client = MongoClient('localhost', 27017)
+db = client['journal_db']
+collection = db['entries']
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    elapsed_time = 0
     if request.method == "POST":
         date = request.form["date"]
         day = request.form["day"]
@@ -21,44 +29,63 @@ def index():
             "model": "gpt-3.5-turbo-0613",
             "messages": [
                 {"role": "system", "content": "Vous êtes un assistant qui aide à remplir un journal de bord, vous n'hésiterez pas à utiliser vos talents d'écritures pour embéllir le tout"},
-                {"role": "user", "content": generate_prompt(date, day, resume)}
-            ],
-            "temperature": temperature
-        }
-
-        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
-        response_data = response.json()
-
-        result = response_data["choices"][0]["message"]["content"].strip()
-        return redirect(url_for("index", result=result))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-def generate_prompt(date, day, resume):
-    return f"""
+                {"role": "user", "content": f"""
     Sur la base des informations fournies : 
     - Date : {date} (format jj/mm/aaa)
     - Jour : {day}
     - Résumé de la journée : {resume}
 
-    Veuillez générer un journal de bord structuré avec des balises HTML pour chaque section, tu utiliseras le format de date jj/mm/aaaa :
+    Veuillez générer un journal de bord au format JSON avec la structure suivante :
 
-    <h2>Jour {day} - {date}</h2>
-    <h3>Résumé de la journée</h3>
-    <p>{resume}</p>
-    <h3>Tâches réalisées</h3>
-    <ul>
-        <li>...</li>
-    </ul>
-    <h3>Problèmes rencontrés</h3>
-    <p>...</p>
-    <h3>Solutions envisagées</h3>
-    <p>...</p>
-    <h3>Prochaines étapes</h3>
-    <p>...</p>
-    
-    """
+    {{
+        "date": "{date}",
+        "day": "{day}",
+        "summary": "...",
+        "tasks_completed": ["...", "..."],
+        "issues_encountered": "...",
+        "solutions_considered": "...",
+        "next_steps": "..."
+    }}
+    """}
+            ],
+            "temperature": temperature
+        }
+
+        start_time = time.time()
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
+        elapsed_time = time.time() - start_time
+
+        response_data = response.json()
+        result = response_data["choices"][0]["message"]["content"].strip()
+
+        entry = {
+            "date": date,
+            "day": day,
+            "resume": resume,
+            "temperature": temperature,
+            "generated_content": result,
+            "execution_time": elapsed_time
+        }
+
+        collection.insert_one(entry)
+        session['form_submitted'] = True
+        return redirect(url_for("index"))
+
+    if 'form_submitted' in session:
+        last_entry = collection.find_one(sort=[('_id', -1)])
+        result = last_entry["generated_content"]
+        elapsed_time = last_entry["execution_time"]
+        session.pop('form_submitted', None)
+    else:
+        result = None
+        elapsed_time = None
+
+    return render_template("index.html", result=result, elapsed_time=elapsed_time)
+
+@app.route("/all_entries")
+def all_entries():
+    entries = collection.find()
+    return render_template("all_entries.html", entries=entries)
 
 if __name__ == "__main__":
     app.run(debug=True)
